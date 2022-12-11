44e5df7c9fdb9bfc9a4e4464f5c0c6c365cfe685
Demo-dot758 <97907711+Demo-dot758@users.noreply.github.com>
2022-12-11 15:22:23 +0100

    Add files via upload

--- trainmodel.py
+++ trainmodel.py
@@ -7,29 +7,30 @@ import os
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical, image_dataset_from_directory
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout, BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras import layers
+from keras import activations
 
 
-
-generateadditional = ImageDataGenerator(rotation_range=10, zoom_range= 0.15, width_shift_range=0.15, height_shift_range=0.15, shear_range=0.1, brightness_range=[1.0,1.2],horizontal_flip=False, vertical_flip=False, fill_mode="nearest",validation_split=0.2)
+generateadditional = ImageDataGenerator(rotation_range=10, zoom_range= 0.2, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.1, brightness_range=[1.0,1.2],horizontal_flip=False, vertical_flip=False, fill_mode="nearest",validation_split=0.2)
 
 train_generator = generateadditional.flow_from_directory(
-    directory='training2/',
+    directory='training_new/',
     target_size=(64, 64),
-    batch_size=32,
+    batch_size=20,
     subset='training',
-    save_to_dir='newdata/')
+    save_to_dir='newdata')
 
 validation_generator = generateadditional.flow_from_directory(
-    directory='training2/',
+    directory='training_new/',
     target_size=(64, 64),
-    batch_size = 8,
+    batch_size = 20,
     subset='validation')
 
 epoch = 100
-filepath = 'detection_newest_64.epoch{epoch:02d}.hdf5'
+filepath = 'model/pro_crop_git12/detection_new.epoch{epoch:02d}.h5'
 checkpoint = ModelCheckpoint(filepath=filepath, 
                              monitor='val_loss',
                              verbose=1, 
@@ -37,23 +38,33 @@ checkpoint = ModelCheckpoint(filepath=filepath,
                              mode='min')
 
 earlystop = EarlyStopping(monitor="val_loss",
-                          patience = 5,
+                          patience = 4,
                           verbose = 1,
                           mode = 'min')
+
 callbacks = [checkpoint, earlystop]
 
 model = Sequential()
 model.add(Conv2D(filters=8, kernel_size=(5,5), activation='relu', input_shape=(64,64,3)))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
 model.add(Conv2D(filters=16, kernel_size=(5,5), activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(rate=0.2))
+model.add(BatchNormalization())
+model.add(Dropout(rate=0.4))
 model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
-model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(rate=0.2))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))    
-model.add(Dense(9, activation='softmax'))
+model.add(Dense(10))  
+model.add(layers.Activation(activations.softmax))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
